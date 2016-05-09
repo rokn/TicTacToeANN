f9a6757b28a36910a8a991bda20701207ab81b0e
Antonio <amindov@abv.bg>
2016-05-09 19:41:32 +0300

    Init

---
+++ src/com/rokner/Main.java
@@ -0,0 +1,8 @@
+package com.rokner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/rokner/NeuralNetwork.java
@@ -0,0 +1,16 @@
+package com.rokner;
+
+import java.util.List;
+
+/**
+ * Created by amind on 5/9/2016.
+ */
+public class NeuralNetwork {
+
+    private double weights[][];
+
+    public NeuralNetwork(List<Integer> topology) {
+        weights = new
+    }
+
+}
